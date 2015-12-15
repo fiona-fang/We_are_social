00eb1028880e94a65a1ca8d8be8cd65b8f60ef5f
fionaflynn <sophie.flynn321@gmail.com>
2015-12-15 15:12:35 +0000

    requirements.txt and setup-libs.sh

---
+++ setup-libs.sh
@@ -0,0 +1,6 @@
+# call this script from the command line like this:
+# $ ./setup-libs.sh
+
+# Install commands 
+sudo apt-get -y install libjpeg-dev zlib1g-dev libmysqlclient-dev
+# add more apt-get install lines below if you need more libs installed
